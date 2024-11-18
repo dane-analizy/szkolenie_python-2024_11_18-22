297d298bab8fdf210ed705b00f0c95f28b75e33e
Łukasz Prokulski <prokulski@gmail.com>
2024-11-18 10:32:01 +0100

    dzien 01

---
+++ dzien_01/main.py
@@ -0,0 +1,38 @@
+# print("Dzień dobry")
+
+# print("Dzień dobry")
+# print("Dzień dobry")
+# print("Dzień dobry")
+# print("Dzień dobry")
+# print("Dzień dobry")
+# print("Dzień dobry")
+
+# for a in range(10):
+#     print("Dzień dobry")
+#     print("Dzień dobry")
+    
+#     for a in range(10):
+#         print("Dzień dobry")
+#         print("Dzień dobry")
+# print("Dzień dobry")
+
+# # nazwy zmiennych
+# kwotapodatku
+# KwotaPodatku
+# kwotaPodatku <- tak lubi Java
+# kwota_podatku # <- tak powinno być w pythonie
+# Kwota_Podatku
+
+# # nazwy stałych
+# KWOTA_PODATKU
+
+
+# nazwt klas
+# class MojaKlasa
+
+# nazwy funkcji
+# def to_jest_funkcja(param1, param2):
+
+# print("kot ma ale")
+# print = 12
+# print("ala ma kota")
