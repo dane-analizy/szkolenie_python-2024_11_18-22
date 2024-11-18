ee51ea9f1961906fb8c316e7fb72af36f967a752
Łukasz Prokulski <prokulski@gmail.com>
2024-11-18 10:58:20 +0100

    dzien 01

--- dzien_01/main.py
+++ dzien_01/main.py
@@ -27,7 +27,7 @@
 # KWOTA_PODATKU
 
 
-# nazwt klas
+# nazwy klas
 # class MojaKlasa
 
 # nazwy funkcji
@@ -36,3 +36,33 @@
 # print("kot ma ale")
 # print = 12
 # print("ala ma kota")
+
+
+# ZMIENNE
+
+# definicje - tworzymy zmienną i nadajemy jej wartość
+# zmienna_int = 123
+# zmienna_float = 3.1415
+# zmienna_znak = "a"
+# zmienna_napis = "ala ma kota"
+
+# # wyświetlamy wartości zmiennych
+# # print(zmienna_int)
+# # print(zmienna_float)
+# # print(zmienna_znak)
+# # print(zmienna_napis)
+
+# print("zmienna_int", zmienna_int)
+# print("zmienna_float", zmienna_float)
+# print("zmienna_znak", zmienna_znak)
+# print("zmienna_napis", zmienna_napis)
+
+# # zmienna_napis = 56789
+# # print("zmienna_napis", zmienna_napis)
+
+# # print(type(zmienna_znak))
+# # print(type(zmienna_napis))
+
+# # zmienna_nowa = zmienna_int + zmienna_float
+# zmienna_nowa = zmienna_float + 10
+# print(type(zmienna_nowa))
