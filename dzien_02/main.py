db5e6e6417aa7c45eab935b13b509a9ca1262688
Łukasz Prokulski <prokulski@gmail.com>
2024-11-19 09:04:40 +0100

    dzien drugi

---
+++ dzien_02/main.py
@@ -0,0 +1,10 @@
+# listy
+
+lista_liczb = [1, 2, 3, 45, 67]
+print(lista_liczb)
+
+lista_napisow = ["abc", "", "cde", "ghijk"]
+print(lista_napisow)
+
+lista_mieszana = [1, 2, 3, "abc", "def", 5, 6, 7]
+print(lista_mieszana)
