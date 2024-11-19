578890856f8968e336359b8e7fa4f3bb8072de8d
Łukasz Prokulski <prokulski@gmail.com>
2024-11-19 09:36:09 +0100

    dzien drugi

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -10,13 +10,53 @@
 # print(lista_mieszana)
 
 # lista może mieć elementy różnego typu
-lista = [1, 2.0, 3, "abc", "def", 5, 6, 7, [1, 2, 3], ["a", "b", "c"]]
-for element in lista:
-    print(element, type(element))
+# lista = [1, 2.0, 3, "abc", "def", 5, 6, 7, [1, 2, 3], ["a", "b", "c"]]
+# for element in lista:
+#     print(element, type(element))
 
-    # czy element jest określonego typu?
-    if isinstance(element, list):
-        print("Element jest listą")
-        for maly_element in element:
-            print("\t", maly_element)
+#     # czy element jest określonego typu?
+#     if isinstance(element, list):
+#         print("Element jest listą")
+#         for maly_element in element:
+#             print("\t", maly_element)
 
+# lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5]
+# konkretny element na liście
+# print(lista[4])
+# print(lista[0])
+
+# jak długa jest lista - ile ma elementów?
+# print(len(lista))
+
+# ostatni element na liście
+# print(lista[len(lista) - 1])
+# print(lista[-1])
+
+# przedostatni element na liście
+# print(lista[len(lista) - 2])
+# print(lista[-2])
+
+# indeks elementu na liście - element musi istnieć
+# print(lista.index(5))
+
+# czy 5 jest na liście?
+# if 5 in lista:
+#     print("piątka jest na liście")
+
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5]
+
+# zakres elementów z listy
+# print(lista[1:4])
+# print(lista[1:100])
+# print(lista[100:2000])
+
+# zakres elementów ale co N-ty element?
+# print(lista[1:100:3])
+
+print(lista[ :5])
+print(lista[5: ])
+print(lista[::2]) # [0:-1:2]
+
+# odwrócenie kolejności listy
+print(lista[::-1])
