4c21d68c157f2e6f59ba67894868a76641ef4976
Łukasz Prokulski <prokulski@gmail.com>
2024-11-20 08:45:20 +0100

    dzień trzeci

--- dzien_03/main.py
+++ dzien_03/main.py
@@ -1,1 +1,2 @@
-# wyszukianie ciągu znaków w plikach  - zadanie z pathlib.Path()
+# wyszukianie ciągu znaków w plikach  - zadanie z pathlib.Path() + szukanie instrukcji pythona
+# https://wolnelektury.pl/media/book/txt/pan-tadeusz.txt
