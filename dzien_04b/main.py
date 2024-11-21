f2bf63dfcdbab4e76d1cd3d5576971bba0f1fc32
Łukasz Prokulski <prokulski@gmail.com>
2024-11-21 13:40:06 +0100

    dzien 4

--- dzien_04b/main.py
+++ dzien_04b/main.py
@@ -1,15 +1,20 @@
 # moduły = zbiory funkcji w oddzielnym pliku
 # zobacz plik "narzedzia.py"
 
-from narzedzia import bmi
-from narzedzia import as_float
+# from narzedzia import bmi
+# from narzedzia import as_float
 
-wynik = bmi(80, 180)
-print(wynik)
+# wynik = bmi(80, 180)
+# print(wynik)
 
-print(as_float("124.515"))
+# print(as_float("124.515"))
 
 
+
+import modul_test
+
+modul_test.hello("Łukasz")
+print(modul_test.zmienna_x)
 
 # __name__
 
---
+++ dzien_04b/modul_dwa.py
@@ -0,0 +1,11 @@
+print("jestem na początku modul_test.py")
+
+zmienna_x = 123
+
+
+def hello(imie):
+    print(f"Witaj {imie}!")
+
+
+hello("Janek")
+print(f"jestem na końcu modul_test.py, a zmienna_x = {zmienna_x}")
