798fc3a624f827799216c47e78950e31345400f8
Łukasz Prokulski <prokulski@gmail.com>
2024-11-21 09:20:41 +0100

    dzien 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -1,4 +1,49 @@
-# jsony
+# jsony -> przykład w pliku dane.json
+
+# wczytanie jsona
+# import json
+
+# with open("dane.json", "r", encoding="utf-8") as fp:
+#     dane = json.load(fp)
+
+# print(dane)
+# print(type(dane))
+
+# print()
+
+# for k,v in dane.items():
+#     print(k,v)
+
+# print()
+
+# for el in dane.get("users"):
+#     print(el)
+
+
+# zapisanie
+# import json
+
+# d = {
+#     "Tom": {"waga": 68, "wzrost": 170},
+#     "Lady": {"waga": 68, "wzrost": 170},
+# }
+
+# with open("dane_2.json", "w", encoding="utf-8") as fp:
+#     json.dump(d, fp)
+#     s = json.dumps(d)
+
+# print(s)
+# print(d)
+
+
+#### ZADANIE 28
+
+# Korzystając z pakietu Faker przygotuj 5 rekordów składających się z: imienia, nazwiska,
+# numeru telefonu osoby.
+# Rekordy umieść na liście, a następnie zapisz każdy z nich (iterując po liście) w pliku JSON,
+# którego nazwa to kolejny numer (1.json, 2.json)
+
+
 # yamle
 # funkcje + moduły
 # requests
