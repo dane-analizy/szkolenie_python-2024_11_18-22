138798059382655bdb5e984574322a7a557959c5
Łukasz Prokulski <prokulski@gmail.com>
2024-11-22 09:23:23 +0100

    dzien 5

---
+++ dzien_05/main.py
@@ -0,0 +1,11 @@
+# https://dbeaver.io/download/
+
+# Tworzymy tabelę w bazie:
+
+# create table players (
+# 	player_id integer primary key auto increment,
+# 	first_name text not null,
+# 	last_name text not null,
+# 	height numeric not null,
+# 	weight numeric not null
+# );
