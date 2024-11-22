a49e61d8799e867e63fee19f5869e6ea9e8bce77
Łukasz Prokulski <prokulski@gmail.com>
2024-11-22 11:44:22 +0100

    dzien 5

--- dzien_05/main.py
+++ dzien_05/main.py
@@ -86,56 +86,94 @@
 # 5. posprzątać: zamknąć bazę, zamknąć plik
 
 
+# from utils.config import load_config
+# from utils.db import disconnect_from_db, open_db, run_db_query
+
+# # CONFIG_FILE = "db_config.yaml"
+# CONFIG_FILE = "db_config_lukasz.yaml"
+
+
+# def db_to_csv(config, nazwa_pliku):
+#     # 1. wczytać konfigurację
+#     config = load_config(config)
+
+#     # 2. połączyć się do bazy danych
+#     db_conn = open_db(config)
+
+#     # 3. pobrać dane z bazy -> zapytanie: "SELECT * FROM players;"
+#     query = """
+#     SELECT
+#         first_name
+#         , weight
+#         , last_name
+#     FROM
+#         players
+#     WHERE
+#         weight < 80
+#     ;
+#     """
+
+#     res = run_db_query(db_conn, query)
+
+#     # 4. wyniki zapisać do pliku (otworzyć plik, sformatować wyniki do postaci rekord = 1 linia, zapisać kolejne linie)
+#     with open(nazwa_pliku, "w", encoding="utf-8") as plik:
+#         for linia in res:
+#             # print(linia)
+#             # linia_zapis = (
+#             #     str(linia[0])
+#             #     + ";"
+#             #     + linia[1]
+#             #     + ";"
+#             #     + linia[2]
+#             #     + ";"
+#             #     + str(linia[3])
+#             #     + ";"
+#             #     + str(linia[4])
+#             # )
+#             linia_zapis = ";".join([str(el) for el in linia])
+#             # print(linia_zapis)
+#             plik.write(linia_zapis + "\n")
+
+#     disconnect_from_db(db_conn)
+#     # 5. posprzątać: zamknąć bazę, zamknąć plik
+
+
+# db_to_csv(CONFIG_FILE, "zawodnicy.csv")
+
+
+# przechodzenie przez dwie listy na raz
+l1 = [1, 2, 3, 4]
+l2 = ["a", "b", "c", "d", "e"]
+
+# for indeks, el1 in enumerate(l1):
+#     print(el1, l2[indeks])
+
+# for indeks, el2 in enumerate(l2):
+#     print(el2, l1[indeks])
+
+# for el2, el1 in zip(l2, l1):
+#     print(el1, el2)
+
+
+
 from utils.config import load_config
 from utils.db import disconnect_from_db, open_db, run_db_query
 
 # CONFIG_FILE = "db_config.yaml"
 CONFIG_FILE = "db_config_lukasz.yaml"
 
+config = load_config(CONFIG_FILE)
+db_conn = open_db(config)
 
-def db_to_csv(config, nazwa_pliku):
-    # 1. wczytać konfigurację
-    config = load_config(config)
+query = "SELECT * FROM players;"
+res = run_db_query(db_conn, query)
 
-    # 2. połączyć się do bazy danych
-    db_conn = open_db(config)
+# nazwy kolumn z wyników
+cols = list(res.keys())
 
-    # 3. pobrać dane z bazy -> zapytanie: "SELECT * FROM players;"
-    query = """
-    SELECT
-        first_name
-        , weight
-        , last_name
-    FROM
-        players
-    WHERE
-        weight < 80
-    ;
-    """
+for record in res:
+    print("="*40)
+    for el, column_name in zip(record, cols):
+        print(f"{column_name} => {el}")
 
-    res = run_db_query(db_conn, query)
-
-    # 4. wyniki zapisać do pliku (otworzyć plik, sformatować wyniki do postaci rekord = 1 linia, zapisać kolejne linie)
-    with open(nazwa_pliku, "w", encoding="utf-8") as plik:
-        for linia in res:
-            # print(linia)
-            # linia_zapis = (
-            #     str(linia[0])
-            #     + ";"
-            #     + linia[1]
-            #     + ";"
-            #     + linia[2]
-            #     + ";"
-            #     + str(linia[3])
-            #     + ";"
-            #     + str(linia[4])
-            # )
-            linia_zapis = ";".join([str(el) for el in linia])
-            # print(linia_zapis)
-            plik.write(linia_zapis + "\n")
-
-    disconnect_from_db(db_conn)
-    # 5. posprzątać: zamknąć bazę, zamknąć plik
-
-
-db_to_csv(CONFIG_FILE, "zawodnicy.csv")
+disconnect_from_db(db_conn)
