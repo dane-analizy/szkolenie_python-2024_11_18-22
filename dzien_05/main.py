41b6491390969da9ce65d3104774bc98e050708b
Łukasz Prokulski <prokulski@gmail.com>
2024-11-22 12:09:59 +0100

    dzien 5

--- dzien_05/main.py
+++ dzien_05/main.py
@@ -155,25 +155,131 @@ l2 = ["a", "b", "c", "d", "e"]
 #     print(el1, el2)
 
 
+# from utils.config import load_config
+# from utils.db import disconnect_from_db, open_db, run_db_query
+# import json
 
-from utils.config import load_config
-from utils.db import disconnect_from_db, open_db, run_db_query
+# # CONFIG_FILE = "db_config.yaml"
+# CONFIG_FILE = "db_config_lukasz.yaml"
 
-# CONFIG_FILE = "db_config.yaml"
-CONFIG_FILE = "db_config_lukasz.yaml"
+# config = load_config(CONFIG_FILE)
+# db_conn = open_db(config)
 
-config = load_config(CONFIG_FILE)
-db_conn = open_db(config)
+# query = "SELECT * FROM players;"
+# res = run_db_query(db_conn, query)
 
-query = "SELECT * FROM players;"
-res = run_db_query(db_conn, query)
+# # nazwy kolumn z wyników
+# cols = list(res.keys())
 
-# nazwy kolumn z wyników
-cols = list(res.keys())
 
-for record in res:
-    print("="*40)
-    for el, column_name in zip(record, cols):
-        print(f"{column_name} => {el}")
+# # lista słowników
+# wyniki = []
+# for record in res:
+#     print("="*40)
 
-disconnect_from_db(db_conn)
+#     record_dict = {}
+#     for el, column_name in zip(record, cols):
+#         # print(f"{column_name} => {el}")
+#         if not isinstance(el, (str, int, float)):
+#             el = str(el)
+#         record_dict[column_name] = el
+
+#     print(record_dict)
+#     wyniki.append(record_dict)
+
+# disconnect_from_db(db_conn)
+
+# with open("dane.json", "w", encoding="utf-8") as f:
+#     json.dump(wyniki, f)
+
+
+# zapis danych do bazy
+
+# INSERT INTO teableka (kolumna, kolumn2, kolumn3) VALUES (1,2,3)
+
+# from utils.config import load_config
+# from utils.db import disconnect_from_db, open_db
+# from sqlalchemy import text
+
+# # CONFIG_FILE = "db_config.yaml"
+# CONFIG_FILE = "db_config_lukasz.yaml"
+
+# config = load_config(CONFIG_FILE)
+# db_conn = open_db(config)
+
+# rekordy = [
+#     {
+#         "imie": "Druga Halina",
+#         "nazwisko": "Kowalska-Bąk",
+#         "wzrost": 1.65,
+#         "waga": 59,
+#     },
+#     {"imie": "Druga Krystyna", "nazwisko": "Góral", "wzrost": 1.71, "waga": 58},
+#     {"imie": "Druga Zosia", "nazwisko": "Iksińska", "wzrost": 1.84, "waga": 65}
+# ]
+
+# for osoba in rekordy:
+#     imie = osoba["imie"]
+#     nazwisko = osoba["nazwisko"]
+#     wzrost = osoba["wzrost"]
+#     waga = osoba["waga"]
+
+#     zapytanie = f"""
+#     INSERT INTO players (first_name, last_name, height, weight)
+#     VALUES ('{imie}', '{nazwisko}', {wzrost}, {waga})
+#     """
+
+#     print(zapytanie)
+#     db_conn.execute(text(zapytanie))
+#     db_conn.commit() # to jest koniecznie przy zapisywaniu zmian do bazy
+
+# disconnect_from_db(db_conn)
+
+
+# nieco inaczej
+
+# from utils.config import load_config
+# from utils.db import disconnect_from_db, open_db
+# from sqlalchemy import text
+
+# # CONFIG_FILE = "db_config.yaml"
+# CONFIG_FILE = "db_config_lukasz.yaml"
+
+# config = load_config(CONFIG_FILE)
+# db_conn = open_db(config)
+
+rekordy = [
+    {
+        "imie": "Trzecia Halina",
+        "nazwisko": "Kowalska-Bąk",
+        "wzrost": 1.65,
+        "waga": 59,
+    },
+    {"imie": "Trzecia Krystyna", "nazwisko": "Góral", "wzrost": 1.71, "waga": 58},
+    {"imie": "Trzecia Zosia", "nazwisko": "Iksińska", "wzrost": 1.84, "waga": 65},
+]
+
+# for osoba in rekordy:
+#     zapytanie = """
+#     INSERT INTO players (first_name, last_name, height, weight)
+#     VALUES (:imie, :nazwisko, :wzrost, :waga)
+#     """
+#     print(osoba)
+#     print(zapytanie)
+#     db_conn.execute(text(zapytanie), osoba )
+#     db_conn.commit()  # to jest koniecznie przy zapisywaniu zmian do bazy
+
+# disconnect_from_db(db_conn)
+
+
+# osoba = rekordy[0]
+# print(osoba)
+# lista_kolumny = ", ".join(osoba.keys())
+# print(lista_kolumny)
+# lista_kluczy = ", ".join( [f":{k}" for k in osoba.keys()])
+# print(lista_kluczy)
+# zapytanie = "INSERT INTO players (" + lista_kolumny + ") VALUES (" + lista_kluczy + ");"
+
+# print(zapytanie)
+
+# db_conn.excute(text(zapytanie), osoba)
