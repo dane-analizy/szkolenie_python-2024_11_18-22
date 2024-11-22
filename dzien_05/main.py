11cdb1aadb62f7559175df98d0ea49a8385c4383
Łukasz Prokulski <prokulski@gmail.com>
2024-11-22 09:30:28 +0100

    dzien 5

--- dzien_05/main.py
+++ dzien_05/main.py
@@ -3,9 +3,30 @@
 # Tworzymy tabelę w bazie:
 
 # create table players (
-# 	player_id integer primary key auto increment,
+# 	player_id serial primary key,
 # 	first_name text not null,
 # 	last_name text not null,
 # 	height numeric not null,
 # 	weight numeric not null
 # );
+
+
+# Wkładamy do niej trochę danych:
+
+# insert into players (first_name,last_name,height,weight)  
+# values ('Jan', 'Kowalski', 184, 85);  
+  
+# insert into players (first_name,last_name,height,weight)  
+# values ('Marian', 'Nowak', 190, 50);  
+  
+# insert into players (first_name,last_name,height,weight)  
+# values ('Zdzisław', 'Dyrman', 173 ,74);  
+  
+# insert into players (first_name,last_name,height,weight)  
+# values ('Zenon', 'Brzęczyk', 164, 95);  
+  
+# insert into players (first_name,last_name,height,weight)  
+# values ('Chuck','Norris', 182, 78);  
+  
+# insert into players (first_name,last_name,height,weight)  
+# values ('Krzysztof','Jarzyna', 168, 70);
